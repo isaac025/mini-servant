e929cb5ccba9ca8d784a16309661be4807869747
Isaac Lopez <isaac.lopez@upr.edu>
2023-11-04 22:32:49 -0400

    finished tutorial

--- src/Main.hs
+++ src/Main.hs
@@ -47,12 +47,39 @@ handleMyApi = handleDate :<|> handleTime
 class HasServer layout where
     route :: Proxy layout -> Server layout -> [String] -> Maybe (IO String)
 
-serve :: HasServer layout => Proxy layout -> Server layout -> [String] -> IO String
-serve p h xs = 
+serve :: (HasServer layout) => Proxy layout -> Server layout -> [String] -> IO String
+serve p h xs =
     case route p h xs of
         Nothing -> ioError (userError "404")
         Just m -> m
 
-    
+instance (Show a) => HasServer (Get a) where
+    route :: Proxy (Get a) -> IO a -> [String] -> Maybe (IO String)
+    route _ handler [] = Just (show <$> handler)
+    route _ _ _ = Nothing
+
+instance (HasServer a, HasServer b) => HasServer (a :<|> b) where
+    route ::
+        Proxy (a :<|> b) ->
+        (Server a :<|> Server b) ->
+        [String] ->
+        Maybe (IO String)
+    route _ (handlera :<|> handlerb) xs =
+        route (Proxy :: Proxy a) handlera xs
+            <|> route (Proxy :: Proxy b) handlerb xs
+
+instance (KnownSymbol s, HasServer r) => HasServer ((s :: Symbol) :> r) where
+    route :: Proxy (s :> b) -> Server r -> [String] -> Maybe (IO String)
+    route _ handler (x : xs)
+        | symbolVal (Proxy :: Proxy s) == x = route (Proxy :: Proxy r) handler xs
+    route _ _ _ = Nothing
+
+instance (Read a, HasServer r) => HasServer (Capture a :> r) where
+    route :: Proxy (Capture a :> b) -> (a -> Server r) -> [String] -> Maybe (IO String)
+    route _ handler (x : xs) = do
+        a <- readMaybe x
+        route (Proxy :: Proxy r) (handler a) xs
+    route _ _ _ = Nothing
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
